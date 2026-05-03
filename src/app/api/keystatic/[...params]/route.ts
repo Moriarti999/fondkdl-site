21a21c4f9a5136957a59704942b1abfd883ee995
Moriarti999 <j12829812@gmail.com>
2026-05-03 09:22:37 +0300

    Fix: correct keystatic.config path in API route

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,5 +1,5 @@
 // 🔥 src/app/api/keystatic/[...params]/route.ts
 import { keystaticNext } from '@keystatic/next/api';
-import config from '../../../../keystatic.config'; 
+import config from '../../../../../keystatic.config'; 
 
 export const { GET, POST } = keystaticNext({ config });
